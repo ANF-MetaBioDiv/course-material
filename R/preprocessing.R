44c6cb69ad06cb2ab503083bb4384d57be274f81
Nicolas Henry <nicolashenry50@gmail.com>
2023-08-24 15:52:00 +0300

    Primer trim with cutadapt The ASV names are also simplified

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -4,6 +4,7 @@
 #' @param filefw R1 file names
 #' @param filefw R2 file names
 #' @param outfile Output pdf file name
+#' @export
 
 qualityprofile <- function(filefw, filerev, outfile) {
   pdf(outfile, onefile = TRUE)
@@ -14,4 +15,88 @@ qualityprofile <- function(filefw, filerev, outfile) {
     print(r)
   }
   dev.off()
+}
+
+#' Primer trimming using cutadapt
+#'
+#' @return pair of gunzipped fastq files (R1 and R2) of primer trimmed sequences
+#' @param forward_files vector of names of forward reads files to trim
+#' @param reverse_files vector of names of reverse reads files to trim
+#' @param primer_fwd forward primer
+#' @param primer_rev reverse primer
+#' @param output_dir folder name where to write trimmed sequences
+#' @param min_size sequences shorter than min_size after trimming are
+#' filtered out
+#' @details Input file names have to end with _R1.fastq.gz or _R2.fastq.gz
+#' @export
+
+primer_trim <- function(forward_files, reverse_files,
+                        primer_fwd, primer_rev,
+                        output_dir, min_size) {
+
+  a <- sub("_R[12].fastq.gz", "", forward_files)
+  b <- sub("_R[12].fastq.gz", "", reverse_files)
+
+  if (!identical(a, b)) {
+    stop("forward_files and reverse_files are not matching")
+  }
+
+  path_to_dada2_log <- here::here(
+    "outputs",
+    "dada2",
+    "log"
+  )
+
+  if (!dir.exists(path_to_dada2_log)) dir.create(path_to_dada2_log)
+
+  invisible(
+    mapply(
+      function(x, y) {
+        run_cutadapt_command(x, y, primer_fwd, primer_rev, output_dir, min_size)
+      },
+      forward_files,
+      reverse_files
+    )
+  )
+
+  all_logs_names <- list.files(
+    path_to_dada2_log,
+    pattern = "_primer_trimming.log",
+    full.names = TRUE
+  )
+
+  all_logs <- lapply(all_logs_names, read.table, sep = "\t")
+
+  all_logs <- Reduce("rbind", all_logs)
+
+  names(all_logs) <- c("sample", "status", "in_reads", "in_bp",
+                       "too_short", "too_long", "too_many_n",
+                       "out_reads", "w/adapters", "qualtrim_bp",
+                       "out_bp", "w/adapters2", "qualtrim2_bp", "out2_bp")
+
+  log_file_export <- file.path(path_to_dada2_log, "primer_trimming.log")
+
+  write.table(all_logs, log_file_export, row.names = FALSE, quote = FALSE)
+
+  invisible(file.remove(all_logs_names))
+}
+
+
+#' A function rather aimed at developers
+#' @description A function that does blabla, blabla.
+#' @keywords internal
+
+run_cutadapt_command <- function(forward_file, reverse_file,
+                                 primer_fwd, primer_rev,
+                                 output_dir, min_size) {
+  command <- paste(
+    "bash bash/primer_trimming.bash",
+    forward_file,
+    reverse_file,
+    primer_fwd,
+    primer_rev,
+    output_dir,
+    min_size
+  )
+  system(command)
 }
---
+++ bash/primer_trimming.bash
@@ -0,0 +1,34 @@
+#!/bin/bash/
+
+FORWARD_IN=$1
+REVERSE_IN=$2
+PRIMER_F=$3
+PRIMER_R=$4
+OUT_DIR=$5
+MIN_LENGTH=$6
+
+FORWARD_OUT="${OUT_DIR}/$(basename ${FORWARD_IN})"
+REVERSE_OUT="${OUT_DIR}/$(basename ${REVERSE_IN})"
+
+SAMPLE=$(basename ${FORWARD_IN/_R[12].fastq.gz/})
+
+LOG="outputs/dada2/log/${SAMPLE}_primer_trimming.log"
+TMP_LOG=$(mktemp --tmpdir=".")
+
+cutadapt \
+	-g ${PRIMER_F} \
+	-G ${PRIMER_R} \
+	--report=minimal \
+	--discard-untrimmed \
+	--minimum-length ${MIN_LENGTH} \
+	--no-indels \
+	-o ${FORWARD_OUT} \
+	-p ${REVERSE_OUT} \
+	${FORWARD_IN} \
+	${REVERSE_IN} 1> ${TMP_LOG}
+
+awk -v a="${SAMPLE}" \
+	'BEGIN {OFS="\t"}; NR==2{print a,$0}' \
+	${TMP_LOG} > ${LOG}
+
+rm -f ${TMP_LOG}
