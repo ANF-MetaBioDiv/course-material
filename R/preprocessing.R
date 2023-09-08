ca99495ae10185ac0aa26cba5370fbb7f56f31c7
Nicolas Henry <nicolashenry50@gmail.com>
2023-09-08 10:40:25 +0200

    fix cutadapt windows

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -109,3 +109,85 @@ run_cutadapt_command <- function(forward_file, reverse_file,
   )
   system(command)
 }
+
+#' A function rather aimed at developers
+#' @description A function that does blabla, blabla.
+#' @keywords internal
+
+run_cutadapt_command_windows <- function(forward_file, reverse_file,
+                                 primer_fwd, primer_rev,
+                                 output_dir, min_size, log_dir, sample_name) {
+
+  command <- paste(
+  "py -m cutadapt",
+  "-g", primer_fwd,
+  "-G", primer_rev,
+	"--discard-untrimmed",
+	"--minimum-length", min_size,
+	"--no-indels",
+	"-o", here::here(output_dir, basename(forward_file)),
+	"-p", here::here(output_dir, basename(reverse_file)),
+	forward_file,
+  reverse_file,
+  "1>",
+  here::here(log_dir, paste0(sample_name, "_primer_trimming.log"))
+  )
+
+  system(command)
+
+}
+
+#' Primer trimming using cutadapt on windows
+#'
+#' @return pair of gunzipped fastq files (R1 and R2) of primer trimmed sequences
+#' @param forward_files vector of names of forward reads files to trim
+#' @param reverse_files vector of names of reverse reads files to trim
+#' @param primer_fwd forward primer
+#' @param primer_rev reverse primer
+#' @param output_dir folder name where to write trimmed sequences
+#' @param min_size sequences shorter than min_size after trimming are
+#' filtered out
+#' @details Input file names have to end with _R1.fastq.gz or _R2.fastq.gz.
+#' 
+#' Cutadapt need to be installed on your system. 
+#' @export
+
+primer_trim_windows <- function(forward_files, reverse_files,
+                        primer_fwd, primer_rev,
+                        output_dir, min_size) {
+
+  a <- sub("_R[12].+fastq.gz", "", forward_files)
+  b <- sub("_R[12].+fastq.gz", "", reverse_files)
+
+  if (!identical(a, b)) {
+    stop("forward_files and reverse_files are not matching")
+  }
+
+  path_to_dada2_log <- here::here(
+    "outputs",
+    "dada2",
+    "log"
+  )
+
+  if (!dir.exists(path_to_dada2_log)) dir.create(path_to_dada2_log)
+
+  invisible(
+    mapply(
+      function(x, y) {
+        run_cutadapt_command_windows(x, y, primer_fwd, primer_rev, output_dir, min_size, path_to_dada2_log, a)
+      },
+      forward_files,
+      reverse_files
+    )
+  )
+
+  all_logs_names <- list.files(
+    path_to_dada2_log,
+    pattern = "_primer_trimming.log",
+    full.names = TRUE
+  )
+
+  all_logs <- lapply(all_logs_names, read.table, sep = "\t", header = TRUE)
+  
+  return(all_logs)
+}
